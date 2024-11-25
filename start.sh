f3590591447750bc8ca755e8008d4c0f91054e7e
Achira Gaming <86994940+achiragaming@users.noreply.github.com>
2024-11-25 13:47:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-./startup-webserver.sh 
+echo "Starting the token generater..."
+./startup-webserver.sh
 
 while ! nc -z localhost 8080; do
+  echo "Waiting for webserver to be ready..."
   sleep 1
 done
 
+echo "Starting Node.js app..."
 npm start
